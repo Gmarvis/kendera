1ce6f36dd9137055f13e3935ec1f10f2453a786d
gmarvis <sgmarvis@gmail.com>
2024-01-16 02:29:45 +0100

    working on the onboarding slides

---
+++ components/Onboarding.tsx
@@ -0,0 +1,45 @@
+import { useState, useRef } from "react";
+import { FlatList, StyleSheet, Text, View, Animated } from "react-native";
+import slides from "../slides";
+import OnboardingItem from "./OnboardingItem";
+
+const Onboarding = () => {
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) => <OnboardingItem item={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          // keyExtractor={(item) => item.id}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            {
+              useNativeDriver: false,
+            }
+          )}
+          scrollEventThrottle={32}
+          // onViewableItemsChanged={viewableItemChanged}
+          viewabilityConfig={viewConfig}
+          // ref={slidesRef}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Onboarding;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2C0C54",
+  },
+});
---
+++ components/OnboardingItem.tsx
@@ -0,0 +1,59 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  useWindowDimensions,
+} from "react-native";
+
+// type Item = {
+//   id: number;
+//   title: string;
+//   description: string;
+//   image: string;
+// };
+
+export default function OnboardingItem({ item }: any) {
+  const { width } = useWindowDimensions();
+  return (
+    <View style={[styles.contatiner, { width }]}>
+      <Image
+        source={item.image}
+        style={[styles?.image, { width, resizeMode: "contain" }]}
+      />
+      <View style={{ flex: 0.3 }}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.description}>{item.description}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  contatiner: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    flex: 0.7,
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "800",
+    fontSize: 28,
+    marginBottom: 10,
+    // color: "#493d8a",
+    color: "white",
+    textAlign: "center",
+    paddingHorizontal: 10,
+  },
+  description: {
+    fontWeight: "500",
+    // color: "#62656b",
+    color: "#dddd",
+
+    textAlign: "center",
+    paddingHorizontal: 20,
+  },
+});
